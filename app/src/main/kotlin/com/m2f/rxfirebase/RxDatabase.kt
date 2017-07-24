91f6bdb3629f9789f906f2886f71af28ee8c8ae1
Marc Moreno <ignis.fatue@gmail.com>
2017-07-24 22:24:39 +0200

    updatet database ans tests

--- app/src/main/kotlin/com/m2f/rxfirebase/RxDatabase.kt
+++ app/src/main/kotlin/com/m2f/rxfirebase/RxDatabase.kt
@@ -4,39 +4,41 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.Query
 import com.google.firebase.database.ValueEventListener
-import io.reactivex.Single
+import io.reactivex.Maybe
 
 /**
  * Created by marc on 24/7/17.
  */
 
-inline fun <reified T> Query.observeSingleValue(): Single<T> {
-    return Single.defer<T> {
-        Single.create { emitter ->
-            if (!emitter.isDisposed) {
-                object : ValueEventListener {
-                    override fun onCancelled(p0: DatabaseError) {
-                        if (!emitter.isDisposed) {
-                            emitter.onError(p0.toException())
-                        }
+inline fun <reified T> Query.observeSingleValue(): Maybe<T> {
+    return Maybe.create { emitter ->
+        if (!emitter.isDisposed) {
+            object : ValueEventListener {
+                override fun onCancelled(p0: DatabaseError) {
+                    if (!emitter.isDisposed) {
+                        emitter.onError(p0.toException())
                     }
+                }
 
-                    override fun onDataChange(p0: DataSnapshot) {
-                        if (!emitter.isDisposed) {
+                override fun onDataChange(p0: DataSnapshot) {
+                    if (!emitter.isDisposed) {
+                        if (p0.exists()) {
                             val value: T? = p0.getValue(T::class.java)
                             value?.let {
                                 emitter.onSuccess(p0.getValue(T::class.java))
                             } ?: emitter.onError(kotlin.ClassCastException("Object emitted is not the same class as ${T::class.simpleName}"))
+                        } else {
+                            emitter.onComplete()
                         }
                     }
-
-                }.let {
-                    emitter.setCancellable { this.removeEventListener(it) }
-                    this.addListenerForSingleValueEvent(it)
                 }
-            }
 
+            }.let {
+                emitter.setCancellable { this.removeEventListener(it) }
+                this.addListenerForSingleValueEvent(it)
+            }
         }
 
     }
+
 }
--- app/src/test/kotlin/com/m2f/rxfirebase/DatabaseTest.kt
+++ app/src/test/kotlin/com/m2f/rxfirebase/DatabaseTest.kt
@@ -1,11 +1,17 @@
 package com.m2f.rxfirebase
 
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.*
+import io.kotlintest.mock.mock
+import io.reactivex.observers.TestObserver
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnitRunner
+
 
 /**
  * Created by marc on 24/7/17.
@@ -13,35 +19,116 @@ import org.mockito.junit.MockitoJUnitRunner
 @RunWith(MockitoJUnitRunner::class)
 class DatabaseTest {
 
-    @Mock private lateinit var mockReference: DatabaseReference
+    @Mock private lateinit var mockDatabase: DatabaseReference
 
     @Mock private lateinit var mockDataSnapshot: DataSnapshot
 
     private val testData = TestData()
 
-    private val testDataList: MutableList<TestData> = mutableListOf()
+    private val testDataList: List<TestData> = listOf(testData)
 
-    private val testDataMap: MutableMap<String, TestData> = mutableMapOf()
+    private val testDataMap: Map<String, TestData> = mapOf("key" to testData)
+
+    @Before fun setup() {
+
+        `when`(mockDataSnapshot.exists()).thenReturn(true)
+        `when`(mockDataSnapshot.getValue(TestData::class.java)).thenReturn(testData)
+        //`when`(mockDataSnapshot.key).thenReturn("key")
+        //`when`(mockDataSnapshot.children).thenReturn(listOf(mockDataSnapshot))
+    }
 
 
     @Test fun observeSingleValueReturnJustOneValue() {
+
+
+        val subs: TestObserver<TestData> = TestObserver()
+        mockDatabase
+                .observeSingleValue<TestData>()
+                .subscribe(subs)
+
+        val argument: ArgumentCaptor<ValueEventListener> = ArgumentCaptor.forClass(ValueEventListener::class.java)
+        verify(mockDatabase).addListenerForSingleValueEvent(argument.capture())
+        argument.value.onDataChange(mockDataSnapshot)
+
+        subs.assertNoErrors()
+                .assertValueCount(1)
+                .assertComplete()
+                .assertResult(testData)
+        subs.dispose()
+
 
     }
 
     @Test fun observeSingleValueWithoutData() {
 
+        val noData: DataSnapshot = mock()
+        `when`(noData.exists()).thenReturn(false)
+
+        val subs: TestObserver<TestData> = TestObserver()
+        mockDatabase
+                .observeSingleValue<TestData>()
+                .subscribe(subs)
+
+        val argument: ArgumentCaptor<ValueEventListener> = ArgumentCaptor.forClass(ValueEventListener::class.java)
+        verify(mockDatabase).addListenerForSingleValueEvent(argument.capture())
+        argument.value.onDataChange(noData)
+
+        subs.assertNoErrors()
+                .assertNoValues()
+                .assertComplete()
+        subs.dispose()
+
+
     }
 
     @Test fun observeSingleValueWithWrongtype() {
 
+        val subs: TestObserver<WrongType> = TestObserver()
+        mockDatabase
+                .observeSingleValue<WrongType>()
+                .subscribe(subs)
+
+        val argument: ArgumentCaptor<ValueEventListener> = ArgumentCaptor.forClass(ValueEventListener::class.java)
+        verify(mockDatabase).addListenerForSingleValueEvent(argument.capture())
+        argument.value.onDataChange(mockDataSnapshot)
+
+        subs.assertError(ClassCastException::class.java)
+                .assertNoValues()
+                .assertNotComplete()
+        subs.dispose()
+
     }
 
     @Test fun observeSingleValue_Disconnected() {
+        val subs: TestObserver<TestData> = TestObserver()
+        mockDatabase
+                .observeSingleValue<TestData>()
+                .subscribe(subs)
 
+        val argument = ArgumentCaptor.forClass(ValueEventListener::class.java)
+        verify(mockDatabase).addListenerForSingleValueEvent(argument.capture())
+        argument.value.onCancelled(DatabaseError.zzbU(DatabaseError.DISCONNECTED))
+
+        subs.assertError(DatabaseException::class.java)
+                .assertNoValues()
+                .assertNotComplete()
+        subs.dispose()
     }
 
     @Test fun observeSingleValue_Failed() {
+        val subs: TestObserver<TestData> = TestObserver()
+        mockDatabase
+                .observeSingleValue<TestData>()
+                .subscribe(subs)
 
+        val argument = ArgumentCaptor.forClass(ValueEventListener::class.java)
+        verify(mockDatabase).addListenerForSingleValueEvent(argument.capture())
+        argument.value.onCancelled(DatabaseError.zzbU(DatabaseError.OPERATION_FAILED))
+
+        subs.assertError(DatabaseException::class.java)
+                .assertNoValues()
+                .assertNotComplete()
+        subs.dispose()
     }
 
     @Test fun testSingleValueEvent() {
